cc3b90e90b9a33882bb36a3a6f81b8ff1ef52fa8
lakal <wadduwage.thilak@gmail.com>
2021-04-12 00:25:07 +0530

    add new routing side navigation

--- src/navigation/vertical/apps.js
+++ src/navigation/vertical/apps.js
@@ -1,4 +1,4 @@
-import { Mail, Clipboard, MessageSquare, CheckSquare, Calendar, FileText, Circle, ShoppingCart, User, Clock, Briefcase } from 'react-feather'
+import { Mail, Clipboard, MessageSquare, CheckSquare, Calendar, FileText, Circle, ShoppingCart, User, Clock, Briefcase, Archive, Book } from 'react-feather'
 
 export default [
   {
@@ -16,55 +16,16 @@ export default [
     icon: <Clock size={20} />,
     navLink: '/apps/todo'
   },
-  
+  {
+    id: 'blog',
+    title: 'Manage Class',
+    icon: <Archive size={20} />,
+    navLink: '/apps/manageclasses/view'
+  },
   {
     id: 'users',
-    title: 'Student',
-    icon: <User size={20} />,
-    children: [
-      {
-        id: 'list',
-        title: 'List',
-        icon: <Circle size={12} />,
-        navLink: '/apps/user/list'
-      },
-      {
-        id: 'view',
-        title: 'View',
-        icon: <Circle size={12} />,
-        navLink: '/apps/user/view'
-      },
-      {
-        id: 'edit',
-        title: 'Edit',
-        icon: <Circle size={12} />,
-        navLink: '/apps/user/edit'
-      },
-      {
-        id: 'blog',
-        title: 'Progress Rep.',
-        icon: <Clipboard />,
-        children: [
-          {
-            id: 'blogList',
-            title: 'List',
-            icon: <Circle />,
-            navLink: '/pages/blog/list'
-          },
-          {
-            id: 'blogDetail',
-            title: 'Detail',
-            icon: <Circle />,
-            navLink: '/pages/blog/detail'
-          }
-          // {
-          //   id: 'blogEdit',
-          //   title: 'Add',
-          //   icon: <Circle />,
-          //   navLink: '/pages/blog/edit'
-          // }
-        ]
-      }
-    ]
+    title: 'Manage Subject',
+    icon: <Book size={20} />,
+    navLink: '/apps/managesubject/view'
   }
 ]
